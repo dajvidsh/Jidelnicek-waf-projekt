c051a02ec07c204cd8133a70f17f5de053ca3ec6
JanK0cZ <honzahvozd@email.cz>
2026-05-03 20:13:40 +0200

    pridani recipe detail stranky

--- app/recipes/[id]/page.tsx
+++ app/recipes/[id]/page.tsx
@@ -3,6 +3,8 @@
 import { useParams} from "next/navigation";
 import PageHeader from "@/app/components/Pageheader";
 import { useEffect, useState } from "react";
+import * as React from "react";
+import {Checkbox} from "@/components/ui/checkbox";
 
 interface RecipeDetailInfo {
     id: number;
@@ -41,14 +43,52 @@ export default function Page() {
     },[id])
 
     if (!recipeDetail) {
-        return <div className="text-center py-20">Loading...</div>;
+        return (
+            <div className="flex flex-col items-center justify-center py-40">
+                <div className="animate-spin rounded-full h-10 w-10 border-b-2 border-[#4A4870] mb-4"></div>
+                <p className="text-slate-500 font-medium">Loading recipe...</p>
+            </div>
+        );
     }
 
 
     return (
-        <div>
-            <PageHeader title="Recipe Details"/>
-            <p>ID je: {id}</p>
+        <div className="bg-white min-h-screen pb-10">
+
+            <PageHeader title={recipeDetail.title} />
+
+            <div className="max-w-3xl mx-auto px-6">
+
+                <div className="w-full mb-8 rounded-2xl overflow-hidden bg-slate-50 shadow-sm">
+                    <img
+                        src={recipeDetail.image}
+                        alt={recipeDetail.title}
+                        className="w-full h-64 md:h-80 object-cover"
+                    />
+                </div>
+
+                <div className="mb-10 flex flex-col gap-4">
+                    {recipeDetail.extendedIngredients?.map((ingredient, index) => (
+                        <div key={`${ingredient.id}-${index}`}>
+                            <label className="flex items-center space-x-3 text-[#4A4870] font-bold text-sm md:text-base cursor-pointer">
+                                <Checkbox className="border-[#4A4870] text-[#4A4870] w-5 h-5" />
+                                <span>{ingredient.original}</span>
+                            </label>
+                        </div>
+                    ))}
+                </div>
+
+                <div>
+                    <h3 className="text-[#4A4870] font-bold text-lg mb-3">Instructions</h3>
+
+                    <div className="text-slate-700 text-sm md:text-base leading-relaxed space-y-4">
+                        {recipeDetail.instructions }
+                            {/*// ? recipeDetail.instructions.replace(/<[^>]*>?/gm, '')*/}
+                            {/*// : "Instrukce pro tento recept bohužel nejsou k dispozici."}*/}
+                    </div>
+                </div>
+
+            </div>
         </div>
     );
 
