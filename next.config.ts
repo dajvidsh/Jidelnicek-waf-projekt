b705c7afa1475912e33db8326f1ba5a3ef15f41c
dajvid137 <david.jezek007@gmail.com>
2026-05-05 11:01:11 +0200

    PWA set up fix recipes

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,8 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  output: 'export',
-  images: {
-    unoptimized: true,
-  },
+  /* config options here */
+  devIndicators: false
 };
 
 export default nextConfig;
